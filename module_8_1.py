ab9ad49c2537bcfead21efae19763ace7d33adb9
sapars <110113627+sapars@users.noreply.github.com>
2024-08-01 15:08:26 +0300

    Add files via upload
    add_everything_up(a, b) принимает a и b, которые могут быть как числами(int, float), так и строками(str).
    TypeError - когда a и b окажутся разными типами (числом и строкой), то возвращать строковое представление этих двух данных вместе (в том же порядке). Во всех остальных случаях выполнять стандартные действия.

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:
+        res = a + b
+    except TypeError:
+        res = str(a) + str(b)
+    return res
+
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
